ec81eff237bbc48a3328b435051dd5323840805e
MBernevig <m.bernevig@student.tudelft.nl>
2024-10-23 21:49:37 +0200

    Add multiple lights support

--- shaders/shader_frag.glsl
+++ shaders/shader_frag.glsl
@@ -8,9 +8,21 @@ layout(std140) uniform Material // Must match the GPUMaterial defined in src/mes
 	float transparency;
 };
 
+struct Light {
+    vec4 position;
+    vec4 color;
+};
+
+layout(std140) uniform lightBuffer {
+    int light_count;
+    Light lights[32];
+};
+
 uniform sampler2D colorMap;
 uniform bool hasTexCoords;
 uniform bool useMaterial;
+
+uniform vec3 cameraPosition;
 
 in vec3 fragPosition;
 in vec3 fragNormal;
@@ -22,8 +34,19 @@ void main()
 {
     vec3 normal = normalize(fragNormal);
 
+    vec3 fullColor;
 
-    if (hasTexCoords)       { fragColor = vec4(texture(colorMap, fragTexCoord).rgb, 1);}
-    else if (useMaterial)   { fragColor = vec4(kd, 1);}
-    else                    { fragColor = vec4(normal, 1); } // Output color value, change from (1, 0, 0) to something else
+    if (hasTexCoords)       { fullColor = vec3(texture(colorMap, fragTexCoord).rgb);}
+    else if (useMaterial)   { fullColor = vec3(kd);}
+    else                    { fragColor = vec4(normal, 1); return;} // Output color value, change from (1, 0, 0) to something else
+
+
+
+    for(int i = 0; i<light_count; i++){
+        vec3 lightDir = normalize(lights[i].position.rgb - fragPosition);
+        float diff = max(dot(normal, lightDir), 0.0);
+        vec3 diffuse = diff * lights[i].color.rgb * fullColor;
+
+        fragColor += vec4(diffuse, 1.f);
+    }
 }
